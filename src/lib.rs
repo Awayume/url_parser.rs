b8c9d8177a3200e92576a9c85692bfc45b60a0df
Awayume <dev@awayume.jp>
2023-09-17 15:13:41 +0900

    [trait] add: add a trait for parsing query

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// SPDX-FileCopyrightText: 2023 Awayume <dev@awayume.jp>
+// SPDX-License-Identifier: Apache-2.0
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use url_parser_trait::QueryParams;
