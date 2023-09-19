98a487e4453886e5596f46545d9424609bac12ca
Awayume <dev@awayume.jp>
2023-09-19 21:27:18 +0900

    [derive] feat: Add support for parsing query params from struct contains arrays

--- url_parser_derive/src/lib.rs
+++ url_parser_derive/src/lib.rs
@@ -10,16 +10,30 @@ use syn::{parse_macro_input, parse_quote, Path, Ident, Item, Type};
 
 #[proc_macro_derive(QueryParams)]
 pub fn derive_query_params(input: TokenStream) -> TokenStream {
-    match parse_macro_input!(input) {
-        Item::Struct(ast) => {
-            let ident: Ident = ast.ident;
-            let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
-            let mut query_generator: TokenStream2 = TokenStream2::new();
-            let option_path: Path = parse_quote!(Option);
-            let vec_path: Path = parse_quote!(Vec);
-            for field in ast.fields {
-                let field_ident: Ident = field.ident.unwrap();
-                if let Type::Path(tpath) = field.ty {
+    if let Item::Struct(ast) = parse_macro_input!(input) {
+        let ident: Ident = ast.ident;
+        let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+        let mut query_generator: TokenStream2 = TokenStream2::new();
+        let option_path: Path = parse_quote!(Option);
+        let vec_path: Path = parse_quote!(Vec);
+        for field in ast.fields {
+            let field_ident: Ident = field.ident.unwrap();
+            match field.ty {
+                Type::Array(tarray) => {
+                    if let Type::Path(_) = *tarray.elem {
+                        query_generator = quote! {
+                            #query_generator
+                            // query: String
+                            let mut val: String = self.#field_ident.iter()
+                                .fold(String::new(), |acc, v| format!("{}{},", acc, v));
+                            val.pop();
+                            query += &format!("{}={}&", stringify!(#field_ident), val);
+                        };
+                    } else {
+                        query_generator = unsupported_field_type_error(&field_ident, query_generator);
+                    }
+                },
+                Type::Path(tpath) => {
                     for seg in tpath.path.segments {
                         println!("{:?}", seg.ident);
                         if seg.ident == option_path.segments[0].ident {  // Option
@@ -47,31 +61,37 @@ pub fn derive_query_params(input: TokenStream) -> TokenStream {
                             };
                         }
                     }
-                 } else {
-                    println!("The type of the field {} does not supported.", field_ident);
-                    query_generator = quote! {
-                        #query_generator
-                        compile_error!("Unsupported field type detected.");
-                    };
+                },
+                _ => {
+                    query_generator = unsupported_field_type_error(&field_ident, query_generator);
+                },
+            }
+        }
+        let expanded: TokenStream2 = quote! {
+            impl #impl_generics url_parser::QueryParams for #ident #ty_generics #where_clause {
+                fn to_query_params(&self) -> String {
+                    let mut query: String = "?".to_string();
+                    #query_generator
+                    query.pop();
+                    query
                 }
             }
-            let expanded: TokenStream2 = quote! {
-                impl #impl_generics url_parser::QueryParams for #ident #ty_generics #where_clause {
-                    fn to_query_params(&self) -> String {
-                        let mut query: String = "?".to_string();
-                        #query_generator
-                        query.pop();
-                        query
-                    }
-                }
-            };
-            expanded.into()
-        },
-        _ => {
-            let expanded: TokenStream2 = quote! {
-                compile_error!("This derive macro can only be used for struct.");
-            };
-            expanded.into()
-        },
+        };
+        expanded.into()
+    } else {
+        let expanded: TokenStream2 = quote! {
+            compile_error!("This derive macro can only be used for struct.");
+        };
+        expanded.into()
     }
+}
+
+
+fn unsupported_field_type_error(field_ident: &Ident, mut query_generator: TokenStream2) -> TokenStream2 {
+    println!("The type of the field {} does not supported.", field_ident);
+    query_generator = quote! {
+        #query_generator
+        compile_error!("Unsupported field type detected.");
+    };
+    query_generator
 }
