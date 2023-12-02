1822c4271c7d5e848bd040ab1142fa24de7eacc6
Awayume <dev@awayume.jp>
2023-12-02 22:49:08 +0900

    add: Add `URL` struct

--- src/url.rs
+++ src/url.rs
@@ -1,7 +1,10 @@
 // SPDX-FileCopyrightText: 2023 Awayume <dev@awayume.jp>
 // SPDX-License-Identifier: Apache-2.0
 
-pub use url_parser_derive::QueryParams;
-pub use url_parser_trait::QueryParams;
+pub struct URL {
 
-pub mod url;
+}
+
+impl URL {
+
+}
